c2fad81cd2e131961ef7e10cbfd32a59f1cc5983
Heiligetomate <heiligetomate@protonmail.com>
2026-04-22 13:00:23 +0200

    const request builder params

---
+++ src/request/parameters.rs
@@ -0,0 +1,10 @@
+pub const CLIENT_NAME: &'static str    = "ANDROID_VR"; 
+pub const CLIENT_VERSION: &'static str = "1.60.19";
+pub const DEVICE_MAKE: &'static str    = "Oculus";
+pub const DEVICE_MODEL: &'static str   = "Quest 2";
+pub const HL: &'static str             = "en";
+pub const GL: &'static str             = "US";
+pub const TIMEZONE: &'static str       = "UTC";
+pub const ANDROID_SDK_VERSION: u16     = 29;
+pub const UTC_OFFSET_MINUTES: u16      = 0; 
+
