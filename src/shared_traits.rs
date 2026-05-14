9b13939dadeceb6b875040e5b9c478d03df632f7
Heiligetomate <heiligetomate@protonmail.com>
2026-05-14 17:12:46 +0200

    moved trait to the request folder

--- src/shared_traits.rs
+++ src/shared_traits.rs
@@ -1,15 +1,6 @@
-use reqwest::RequestBuilder;
-
 pub trait Response {
     fn get_visitor_data(&self) -> Option<String>;
     fn get_status(&self) -> Status;
-}
-
-pub type ClientPrebuild = RequestBuilder;
-
-pub trait ClientWithHeaders {
-    type Response: Response;
-    fn get_client() -> ClientPrebuild;
 }
 
 #[derive(PartialEq, Eq, Debug)]
