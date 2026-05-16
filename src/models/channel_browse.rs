dfd69641f7c817fba0417f3ba54d08a73caaa849
Heiligetomate <heiligetomate@protonmail.com>
2026-05-16 21:05:28 +0200

    extract the ids

--- src/models/channel_browse.rs
+++ src/models/channel_browse.rs
@@ -1,7 +1,11 @@
 use serde::Deserialize;
 
-use crate::models::response::{Response, Status};
-// TODO: some should be Option<>.
+use crate::{
+    Result,
+    error::YtuwuError,
+    id_resolver::{channel_playlist_id::ChannelPlaylistId, id::Id},
+    models::response::{Response, Status},
+};
 
 #[derive(Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
@@ -162,6 +166,82 @@ struct Subtitle {
 #[derive(Deserialize, Debug)]
 struct SubtitleRun {
     text: Option<String>,
+}
+
+pub enum ChannelReleaseType {
+    Album,
+    Single,
+    Ep,
+}
+
+#[derive(Debug)]
+pub struct ChannelIdCollection {
+    pub albums: Vec<ChannelPlaylistId>,
+    pub eps: Vec<ChannelPlaylistId>,
+    pub singles: Vec<ChannelPlaylistId>,
+}
+
+impl ChannelBrowseResponse {
+    pub fn extract_all_releases(&self) -> Result<ChannelIdCollection> {
+        let mut albums: Vec<ChannelPlaylistId> = Vec::new();
+        let mut eps: Vec<ChannelPlaylistId> = Vec::new();
+        let mut singles: Vec<ChannelPlaylistId> = Vec::new();
+
+        let contents = &self
+            .contents
+            .as_ref()
+            .ok_or(YtuwuError::BrowseDataNotFound("contents"))?;
+
+        let tab = &contents
+            .single_column_browse_results_renderer
+            .tabs
+            .get(0)
+            .ok_or(YtuwuError::BrowseDataNotFound("tab"))?;
+
+        let section_list = &tab
+            .tab_renderer
+            .content
+            .section_list_renderer;
+
+        let section = &section_list
+            .contents
+            .as_ref()
+            .ok_or(YtuwuError::BrowseDataNotFound("contents"))?
+            .get(0)
+            .ok_or(YtuwuError::BrowseDataNotFound("contents is empty"))?;
+
+        for item in &section.grid_renderer.items {
+            let renderer = &item.music_two_row_item_renderer;
+
+            let browse_id: &str = match &renderer
+                .navigation_endpoint
+                .browse_endpoint
+                .browse_id
+            {
+                Some(id) => &id,
+                None => continue,
+            };
+
+            let playlist_id = ChannelPlaylistId::new(browse_id);
+
+            let type_text = renderer
+                .subtitle
+                .as_ref()
+                .and_then(|s| s.runs.first())
+                .and_then(|r| r.text.as_deref())
+                .unwrap_or("")
+                .to_lowercase();
+
+            match type_text.as_str() {
+                "album" => albums.push(playlist_id),
+                "ep" => eps.push(playlist_id),
+                "single" => singles.push(playlist_id),
+                _ => {}
+            }
+        }
+
+        Ok(ChannelIdCollection { albums, eps, singles })
+    }
 }
 
 impl Response for ChannelBrowseResponse {
