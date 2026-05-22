1db0b82c5c2b181ebf11f2d218b105e21cb576c5
Heiligetomate <heiligetomate@protonmail.com>
2026-05-22 10:21:35 +0200

    laptop -> pc

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,15 @@
-use std::path::Path;
-
 use ytuwu::{
-    Downloader, Result, ThumbnailResolution,
-    id_resolver::{id::GetId, id_collection::IdCollection},
-    itag::{AudioItag, Itag, LongVideoItag},
+    Result,
+    id_resolver::{
+        channel_name_id::ChannelNameId,
+        id::{Id, MakeChannelId},
+    },
 };
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let media_url = "https://music.youtube.com/watch?v=lndG8BiZCmM";
-    let id_collection = IdCollection::from_url(media_url)?;
-
-    let downloader = Downloader::new();
-    let media = downloader
-        .download_dual_media_stream(id_collection.get_id()?, LongVideoItag::highest(), AudioItag::highest(), ThumbnailResolution::Low)
-        .await?;
-    let path = Path::new("teehee");
-    println!("title: {}", media.metadata.title);
-    media.save(&path)?;
-
+    let id = ChannelNameId::new("@ntomusic");
+    let transformed = id.transform().await?;
+    println!("{:#?}", transformed);
     Ok(())
 }
--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,2 +1,3 @@
+mod id_resolver;
 mod models;
 mod request;
