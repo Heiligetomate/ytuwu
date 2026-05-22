eb2abccee4ae11f8ef1a9bae90a53c675d1ddc9a
Heiligetomate <heiligetomate@protonmail.com>
2026-05-22 10:10:17 +0200

    fix wrong get_id fn

---
+++ src/models/channel_name_to_id.rs
@@ -0,0 +1,67 @@
+use serde::Deserialize;
+
+use crate::{
+    Result,
+    error::YtuwuError,
+    id_resolver::{channel_id::ChannelId, id::Id},
+    models::response::Response,
+};
+
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ChannelNameToIdResponse {
+    response_context: Option<ResponseContext>,
+    endpoint: Option<Endpoint>,
+}
+
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct ResponseContext {
+    visitor_data: Option<String>,
+}
+
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct Endpoint {
+    browse_endpoint: Option<BrowseEndpoint>,
+}
+
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct BrowseEndpoint {
+    browse_id: Option<String>,
+}
+
+impl ChannelNameToIdResponse {
+    pub fn get_id(&self) -> Result<ChannelId> {
+        let id: &str = &self
+            .endpoint
+            .as_ref()
+            .ok_or(YtuwuError::ChannelDataNotFound("endpoint"))?
+            .browse_endpoint
+            .as_ref()
+            .ok_or(YtuwuError::ChannelDataNotFound("browse endpoint"))?
+            .browse_id
+            .as_ref()
+            .ok_or(YtuwuError::ChannelDataNotFound("channel id"))?;
+        Ok(ChannelId::new(id))
+    }
+}
+
+impl Response for ChannelNameToIdResponse {
+    fn get_status(&self) -> super::response::Status {
+        if self.endpoint.is_some() {
+            return super::response::Status::Success;
+        }
+        super::response::Status::Error
+    }
+
+    fn get_visitor_data(&self) -> Option<String> {
+        if let Some(response_context) = &self.response_context {
+            if let Some(visitor_data) = &response_context.visitor_data {
+                return Some(visitor_data.to_owned());
+            }
+        }
+        None
+    }
+}
--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,4 +1,5 @@
 pub mod channel_browse;
+pub mod channel_name_to_id;
 mod constants;
 pub mod fast_browse;
 pub mod itag;
