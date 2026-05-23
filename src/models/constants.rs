5d8cf881912042b6559e4fd8d791ff12637e1a83
Heiligetomate <heiligetomate@protonmail.com>
2026-05-24 01:30:52 +0200

    remove useless constants file

--- src/models/constants.rs
+++
@@ -1,3 +0,0 @@
-pub const WEBM_FORMAT: &str = "webm";
-pub const MP4_FORMAT: &str = "mp4";
-pub const M4A_FORMAT: &str = "m4a";
--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,6 +1,5 @@
 pub mod channel_browse;
 pub mod channel_name_to_id;
-mod constants;
 pub mod fast_browse;
 pub mod itag;
 pub mod player;
