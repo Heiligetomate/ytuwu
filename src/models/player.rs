f7b9f2a8f277b442432b55780cef8b56a5aae0eb
Heiligetomate <heiligetomate@protonmail.com>
2026-05-23 18:48:35 +0200

    clean up

--- src/models/player.rs
+++ src/models/player.rs
@@ -1,8 +1,8 @@
 use serde::Deserialize;
 
 use crate::{
+    downloader::media::extracted_streams::ExtractedPlayerResponse,
     error::{Result, YtuwuError},
-    models::itag::Itag,
     models::response::{Response, Status},
 };
 
@@ -17,21 +17,21 @@ pub struct PlayerResponse {
 
 #[derive(Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
-pub struct ResponseContext {
+struct ResponseContext {
     pub visitor_data: Option<String>,
 }
 
 #[derive(Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
-pub struct StreamingData {
+struct StreamingData {
     formats: Vec<Stream>,
     adaptive_formats: Vec<Stream>,
 }
 
 #[derive(Deserialize, Debug)]
 pub struct Stream {
-    itag: u16,
-    url: String,
+    pub itag: u16,
+    pub url: String,
 }
 
 #[derive(Deserialize, Debug)]
@@ -43,26 +43,18 @@ pub struct VideoDetails {
 
 #[derive(Deserialize, Debug)]
 pub struct Thumbnails {
-    thumbnails: Vec<Thumbnail>,
+    pub thumbnails: Vec<ThumbnailStream>,
 }
 
 #[derive(Deserialize, Debug)]
-pub struct Thumbnail {
-    url: String,
-    width: u16,
+pub struct ThumbnailStream {
+    pub url: String,
+    pub width: u16,
 }
 
 #[derive(Deserialize, Debug)]
-pub struct PlayabilityStatus {
+struct PlayabilityStatus {
     pub status: PlayabilityStatusValue,
-}
-
-#[derive(Debug, PartialEq, Eq, Clone)]
-pub enum ThumbnailResolution {
-    Low,
-    Medium,
-    High,
-    VeryHigh,
 }
 
 #[derive(Deserialize, Debug, Clone, Copy, PartialEq, Eq)]
@@ -76,93 +68,20 @@ pub enum PlayabilityStatusValue {
     ContentCheckRequired,
 }
 
-impl ThumbnailResolution {
-    pub fn from_width(width: u16) -> Option<Self> {
-        match width {
-            120 => Some(Self::Low),
-            320 => Some(Self::Medium),
-            480 => Some(Self::High),
-            640 => Some(Self::VeryHigh),
-            _ => None,
-        }
-    }
-}
-
-impl Thumbnails {
-    pub fn url_by_resolution(&self, resolution: &ThumbnailResolution) -> Option<&str> {
-        for thumbnail in self.thumbnails.iter() {
-            if let Some(thumbnail_resolution) = ThumbnailResolution::from_width(thumbnail.width) {
-                if thumbnail_resolution == *resolution {
-                    return Some(&thumbnail.url);
-                }
-            } else {
-                continue;
-            }
-        }
-        None
-    }
-}
-
 impl PlayerResponse {
-    pub fn get_best_stream<I: Itag + Copy>(&self, itag: &I) -> Result<&str> {
-        let streams = self.get_streaming_data()?;
-        let mut current_itag = *itag;
-        let mut url: Option<&str> = streams.get_url_by_itag(&current_itag);
-        while url.is_none() {
-            current_itag = current_itag.next_best()?;
-            url = streams.get_url_by_itag(&current_itag);
-        }
-        Ok(url.ok_or(YtuwuError::NoMatchingItag)?)
-    }
-
-    fn get_streaming_data(&self) -> Result<&StreamingData> {
-        Ok(self
+    pub fn extract(self) -> Result<ExtractedPlayerResponse> {
+        let streaming_data = self
             .streaming_data
-            .as_ref()
-            .ok_or(YtuwuError::PlayerDataNotFound("streaming data"))?)
-    }
+            .ok_or(YtuwuError::PlayerDataNotFound("Streaming data"))?;
 
-    fn get_video_deatails(&self) -> Result<&VideoDetails> {
-        Ok(self
+        let mut media_streams = streaming_data.formats;
+        media_streams.extend(streaming_data.adaptive_formats);
+
+        let video_details = self
             .video_details
-            .as_ref()
-            .ok_or(YtuwuError::PlayerDataNotFound("video details"))?)
-    }
+            .ok_or(YtuwuError::PlayerDataNotFound("video details"))?;
 
-    pub fn get_thumbnail_url_by_res(&self, resolution: &ThumbnailResolution) -> Result<&str> {
-        let url = self
-            .get_video_deatails()?
-            .thumbnail
-            .url_by_resolution(resolution)
-            .ok_or(YtuwuError::PlayerDataNotFound("thumbnails"))?;
-        Ok(url)
-    }
-
-    pub fn get_title(&self) -> Result<&str> {
-        Ok(&self.get_video_deatails()?.title)
-    }
-
-    pub fn get_author(&self) -> Result<&str> {
-        if let Some(video_details) = &self.video_details {
-            return Ok(&video_details.author);
-        }
-        Err(YtuwuError::PlayerDataNotFound("author"))
-    }
-}
-
-impl StreamingData {
-    pub fn get_url_by_itag(&self, itag: &impl Itag) -> Option<&str> {
-        for format in self.adaptive_formats.iter() {
-            if format.itag == itag.to_int() {
-                return Some(&format.url);
-            }
-        }
-        for adaptive_format in self.formats.iter() {
-            if adaptive_format.itag == itag.to_int() {
-                return Some(&adaptive_format.url);
-            }
-        }
-        None
+        Ok(ExtractedPlayerResponse::new(media_streams, video_details))
     }
 }
 
