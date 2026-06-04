b3c312ed2772b78f840d06af6a81d0c9af693a57
Heiligetomate <heiligetomate@protonmail.com>
2026-06-04 14:33:16 +0200

    cleaner dummy, panics instantly

--- src/request/clients/dummy.rs
+++ src/request/clients/dummy.rs
@@ -8,28 +8,11 @@ pub struct DummyClient {}
 impl ClientWithHeaders for DummyClient {
     type Response = DummyResponse;
 
-    fn build_headers(client: &reqwest::Client) -> ClientPrebuild {
+    fn build_headers(_: &reqwest::Client) -> ClientPrebuild {
         panic!("this is a dummy and should never be called");
     }
 
     fn build_body<'de>(_: &str, _: Option<String>) -> super::body::RequestBody<'de> {
-        super::body::RequestBody {
-            context: super::body::Context {
-                client: super::body::ClientBody {
-                    client_name: "",
-                    client_version: "",
-                    device_make: None,
-                    device_model: None,
-                    hl: "",
-                    gl: "",
-                    time_zone: "",
-                    utc_offset_minutes: "",
-                    visitor_data: None,
-                },
-            },
-            video_id: None,
-            browse_id: None,
-            url: None,
-        }
+        panic!("this is a dummy and should never be called");
     }
 }
