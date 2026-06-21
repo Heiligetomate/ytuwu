9126970a7f99ac7c7506e2d5da1ba8ec699a39ce
Heiligetomate <heiligetomate@protonmail.com>
2026-06-21 12:28:18 +0200

    make pub only in super

--- src/request/clients/endpoints.rs
+++ src/request/clients/endpoints.rs
@@ -1,3 +1,4 @@
-pub const PLAYER_ENDPOINT: &str = "https://music.youtube.com/youtubei/v1/player?prettyPrint=false";
-pub const BROWSE_ENDPOINT: &str = "https://music.youtube.com/youtubei/v1/browse?prettyPrint=false";
-pub const RESOLVE_CHANNEL_NAME_ENDPOINT: &str = "https://music.youtube.com/youtubei/v1/navigation/resolve_url?prettyPrint=false";
+/// This
+pub(super) const PLAYER_ENDPOINT: &str = "https://music.youtube.com/youtubei/v1/player?prettyPrint=false";
+pub(super) const BROWSE_ENDPOINT: &str = "https://music.youtube.com/youtubei/v1/browse?prettyPrint=false";
+pub(super) const RESOLVE_CHANNEL_NAME_ENDPOINT: &str = "https://music.youtube.com/youtubei/v1/navigation/resolve_url?prettyPrint=false";
