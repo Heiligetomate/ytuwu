942612cfde8adebcdfe1e4dd8a571a6fef711dcf
Heiligetomate <heiligetomate@protonmail.com>
2026-06-21 12:34:05 +0200

    make pub in super

--- src/request/clients/shared_params.rs
+++ src/request/clients/shared_params.rs
@@ -1,18 +1,18 @@
 type Header = (&'static str, &'static str);
 
-// Client params
-pub const HL: &str = "en";
-pub const GL: &str = "US";
-pub const TIME_ZONE: &str = "UTC";
-pub const UTC_OFFSET_MINUTES: &str = "0";
+/// Host language client parameter
+pub(super) const HL: &str = "en";
+pub(super) const GL: &str = "US";
+pub(super) const TIME_ZONE: &str = "UTC";
+pub(super) const UTC_OFFSET_MINUTES: &str = "0";
 
 // Header names
-pub const ORIGIN_HEADER_NAME: &str = "Origin";
-pub const CONTENT_TYPE_HEADER_NAME: &str = "Content-Type";
-pub const CLIENT_VERSION_HEADER_NAME: &str = "X-YouTube-Client-Version";
-pub const CLIENT_NAME_HEADER_NAME: &str = "X-YouTube-Client-Name";
-pub const USER_AGENT_HEADER_NAME: &str = "User-Agent";
+pub(super) const ORIGIN_HEADER_NAME: &str = "Origin";
+pub(super) const CONTENT_TYPE_HEADER_NAME: &str = "Content-Type";
+pub(super) const CLIENT_VERSION_HEADER_NAME: &str = "X-YouTube-Client-Version";
+pub(super) const CLIENT_NAME_HEADER_NAME: &str = "X-YouTube-Client-Name";
+pub(super) const USER_AGENT_HEADER_NAME: &str = "User-Agent";
 
 // Headers
-pub const ORIGIN_HEADER: Header = (ORIGIN_HEADER_NAME, "https://www.youtube.com");
-pub const CONTENT_TYPE_HEADER: Header = (CONTENT_TYPE_HEADER_NAME, "application/json");
+pub(super) const ORIGIN_HEADER: Header = (ORIGIN_HEADER_NAME, "https://www.youtube.com");
+pub(super) const CONTENT_TYPE_HEADER: Header = (CONTENT_TYPE_HEADER_NAME, "application/json");
