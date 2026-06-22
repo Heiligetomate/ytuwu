29eb7f95af0f501d6f064596cb4e7519aa580e2d
Heiligetomate <heiligetomate@protonmail.com>
2026-06-22 14:30:39 +0200

    metadata documentaiton

--- src/downloader/metadata.rs
+++ src/downloader/metadata.rs
@@ -1,22 +1,30 @@
+/// This metadata is contained in every downloaded media and contains publically exposed data that
+/// give general information about the media.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct MediaMetadata {
     pub title: String,
     pub author: String,
 }
 
+/// This metadata is contained in every downloaded playlist and contains publically exposed data that
+/// give general information about the playlist.
 #[derive(Debug)]
 pub struct PlaylistMetadata {
     pub title: String,
+    // TODO: maybe actually add the author
     //pub author: String,
     pub song_count: u16,
 }
 
+/// This metadata is contained in every downloaded channel and contains publically exposed data that
+/// give general information about the channel.
 #[derive(Debug)]
 pub struct ChannelMetadata {
     pub name: String,
 }
 
 impl MediaMetadata {
+    /// takes ownership of the title and the autor and returns a new instance of MediaMetadata
     pub fn new(title: &str, author: &str) -> Self {
         Self {
             title: title.to_owned(),
@@ -26,12 +34,14 @@ impl MediaMetadata {
 }
 
 impl PlaylistMetadata {
+    /// Takes ownership of the title and creates a new instance of PlaylistMetadata
     pub fn new(title: &str, song_count: u16) -> Self {
         Self { title: title.to_owned(), song_count }
     }
 }
 
 impl ChannelMetadata {
+    /// Takes ownership of the title and creates a new instance of ChannelMetadata
     pub fn new(name: &str) -> Self {
         Self { name: name.to_owned() }
     }
