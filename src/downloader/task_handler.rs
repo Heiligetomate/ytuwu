43a41d09c1c51d0c2d8047eae1af8773df6abf07
Heiligetomate <heiligetomate@protonmail.com>
2026-06-22 15:22:34 +0200

    task handler documentaiotnasd

--- src/downloader/task_handler.rs
+++ src/downloader/task_handler.rs
@@ -17,6 +17,12 @@ use crate::{
 /// slower downloads or even full abortion of the download
 const MAX_TASKS: usize = 4;
 
+/// The task handler is one of the main fields on the downloader
+/// Its task is to store all tasks that have to be worked
+/// This includes downloading media and bundle media
+/// It also holds a Semaphore to ensure that only a specific number of tasks run parallel
+/// The basic funciton is to push tasks and work tasks. As soon as the tasks get worked, they are
+/// getting moved into the downloader storage.
 #[derive(Debug)]
 pub struct TaskHandler {
     tasks: Vec<DownloadTask>,
@@ -25,6 +31,8 @@ pub struct TaskHandler {
 }
 
 impl Default for TaskHandler {
+    /// New default task handler containign two empty task lists and a semaphore with the MAX_TASKS
+    /// as limit
     fn default() -> Self {
         Self {
             tasks: Vec::new(),
@@ -35,16 +43,22 @@ impl Default for TaskHandler {
 }
 
 impl TaskHandler {
+    /// Pushed one task on the TaskHandler tasks vec
     pub fn push(&mut self, video_id: VideoId, playlist_id: Option<Uuid>, channel_id: Option<Uuid>, id: Uuid, itag: AnyItag) {
         let task = DownloadTask::new(itag, id, video_id, playlist_id, channel_id);
         self.tasks.push(task);
     }
 
+    /// Pushes one bundle task on the TaskHandler bundle vec
     pub fn push_bundle(&mut self, video_id: VideoId, playlist_id: Option<Uuid>, channel_id: Option<Uuid>, id: Uuid, itags: &'static [AnyItag]) {
         let task = BundleDownloadTask::new(itags, id, video_id, playlist_id, channel_id);
         self.bundle_tasks.push(task);
     }
 
+    /// Works all bundle tasks and all normal tasks where only MAX_TASKS are running parallel
+    /// All results that were not successful will just be ignored
+    /// Pushes all BundleDownloadTasks and DownloadTask to the downloader storage where they can
+    /// then be extracted with the correct ids.
     pub async fn work(self, downloader: Arc<Downloader>) {
         // TODO: error handling, return a result?
 
